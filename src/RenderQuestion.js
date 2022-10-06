99a7ef9e3b9521d7d26d1fe1809d38c801c92d35
unknown <harish.16634@gmail.com>
2022-10-06 18:22:03 +0530

    first commit

--- src/RenderQuestion.js
+++ src/RenderQuestion.js
@@ -2,30 +2,7 @@ import { useEffect, useState } from "react"
 import RenderButton from "./RenderButton"
 
 export default function(props) {
-    //let options = [props.correctAnswer, ...props.wrongAnswer]
-    let options = shuffle([props.correctAnswer, ...props.wrongAnswer])
-
-    useEffect(() => {
-        options = shuffle([props.correctAnswer, ...props.wrongAnswer])
-    }, [])
-
-    function shuffle(array) {
-        let currentIndex = array.length,  randomIndex;
-
-        // While there remain elements to shuffle.
-        while (currentIndex != 0) {
-
-            // Pick a remaining element.
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex--;
-
-            // And swap it with the current element.
-            [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
-        }
-
-        return array;
-    }
+    let options = [props.correctAnswer, ...props.wrongAnswer]
 
     const keyOptions = props.options
     let key = Math.random()
